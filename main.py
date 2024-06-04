45c0aa23d3e8832471832ea9f1eb51c648c4c1ad
FathanAkram-App <fathan.a.dev@gmail.com>
2024-06-04 13:29:53 +0700

    some more detail simulation

--- main.py
+++ main.py
@@ -16,12 +16,30 @@ class Block:
     def getBlockHash(self):
             return self.blockhash
     
-blockchain = frozenset()
+blockchain = []
 def main():
     transaction = ("jack has sent fathan 10 bitcoin","max has sent fathan 10 bitcoin")
     someBlock = Block(0, transaction=transaction)
+    blockchain.append(someBlock)
 
+    transaction = ("mark has sent fathan 1000 bitcoin","max has sent fathan 102 bitcoin")
+    someBlock2 = Block(someBlock.getBlockHash(), transaction=transaction)
+    blockchain.append(someBlock2)
+
+    transaction = ("jeff besos has sent fathan 1000 bitcoin")
+    someBlock3 = Block(someBlock.getBlockHash(), transaction=transaction)
+    blockchain.append(someBlock3)
+
+    print("first transaction:")
     print(someBlock.getBlockHash())
+    print("second transaction:")
+    print(someBlock2.getBlockHash())
+    print("second transaction:")
+    print(someBlock3.getBlockHash())
+    print("___________________")
+    print("blockchain :")
+    for v in blockchain:
+        print(v.getBlockHash())
 
 if __name__ == "__main__":
     main()
