0f0d32a880af3f17cf0a250afe5f00d5ba3bd6cf
FathanAkram-App <fathan.a.dev@gmail.com>
2024-06-04 13:06:17 +0700

    blockchain simulation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A Simple BlockChain Made With Python
+
+this is only for understanding how blockchain works, not for production
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello")
+class Block:
+
+    def __init__(self, previousHash, transaction):
+        self.previousHash = previousHash
+        self.transaction = transaction
+
+        contents = {hash(transaction), previousHash}
+        self.blockhash = hash(frozenset(contents))
+        
+    def getPreviousHash(self):
+        return self.previousHash
+    
+    def getTransaction(self):
+            return self.transaction
+    
+    def getBlockHash(self):
+            return self.blockhash
+    
+blockchain = frozenset()
+def main():
+    transaction = ("jack has sent fathan 10 bitcoin","max has sent fathan 10 bitcoin")
+    someBlock = Block(0, transaction=transaction)
+
+    print(someBlock.getBlockHash())
+
+if __name__ == "__main__":
+    main()
+
+
